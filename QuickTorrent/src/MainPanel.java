e092a05d26075c2397f55c39750244dfdd1a6ce0
JaminB <jaminbecker@gmail.com>
2013-08-05 17:34:00 -0400

    Added a show console button, and added functionality to radio buttons that report status to the console window.

--- QuickTorrent/src/MainPanel.java
+++ QuickTorrent/src/MainPanel.java
@@ -2,48 +2,47 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import javax.swing.JToolBar;
-import javax.swing.JList;
 import javax.swing.JTextField;
-import javax.swing.JEditorPane;
-import javax.swing.JTextArea;
-import javax.swing.DropMode;
 import javax.swing.JTextPane;
+
 import java.awt.GridLayout;
-import javax.swing.JSplitPane;
-import javax.swing.JTable;
-import java.awt.FlowLayout;
+
 import javax.swing.JScrollPane;
-import javax.swing.JComboBox;
-import javax.swing.BoxLayout;
-import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
-import java.awt.Component;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+
 import java.awt.Font;
-import javax.swing.JLabel;
+
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JDesktopPane;
 import javax.swing.JCheckBox;
-import javax.swing.JMenu;
 import javax.swing.JRadioButton;
 import javax.swing.AbstractAction;
+
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.Action;
-import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JSeparator;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
+
 import java.awt.SystemColor;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
 
 
 public class MainPanel {
+	public JTextPane textPane;
+	public JCheckBox chckbxFilterResults;
+	public JRadioButton rdbtnAlbum;
+	public JRadioButton rdbtnSingle;
+	public JRadioButton rdbtnMovie;
+	public StyledDocument doc;
+	public boolean consoleOn = true;
+	public String displayText = "Welcome to Quick Torrent!\n";
 
 	private JFrame frmQuickTorrent;
-	private JTextField textField;
-	private final Action action = new SwingAction();
+	private JTextField searchBox;
 
 	/**
 	 * Launch the application.
@@ -81,28 +80,32 @@ public class MainPanel {
 		JPanel panel = new JPanel();
 		frmQuickTorrent.getContentPane().add(panel);
 		
-		textField = new JTextField();
-		textField.setHorizontalAlignment(SwingConstants.LEFT);
-		textField.setFont(new Font("OCR A Extended", Font.BOLD, 11));
-		textField.setColumns(15);
+		searchBox = new JTextField();
+		searchBox.setHorizontalAlignment(SwingConstants.LEFT);
+		searchBox.setFont(new Font("OCR A Extended", Font.BOLD, 11));
+		searchBox.setColumns(15);
 		
-		JCheckBox chckbxFilterResults = new JCheckBox("Filter Results");
+		chckbxFilterResults = new JCheckBox("Filter Results");
 		chckbxFilterResults.setFont(new Font("OCR A Extended", Font.PLAIN, 13));
 		
-		JButton btnSearch = new JButton("Search");
-		btnSearch.setFont(new Font("OCR A Extended", Font.PLAIN, 13));
-		
+		JButton searchButton = new JButton("Search");
+		searchButton.setFont(new Font("OCR A Extended", Font.PLAIN, 13));
 		JSeparator separator = new JSeparator();
 		separator.setForeground(SystemColor.desktop);
 		
-		JRadioButton rdbtnMovie = new JRadioButton("Single");
-		rdbtnMovie.setFont(new Font("OCR A Extended", Font.PLAIN, 11));
+		rdbtnSingle = new JRadioButton("Single");
+		rdbtnSingle.setSelected(true);
+		rdbtnSingle.setFont(new Font("OCR A Extended", Font.PLAIN, 11));
 		
-		JRadioButton rdbtnAlbum = new JRadioButton("Album");
+		rdbtnAlbum = new JRadioButton("Album");
 		rdbtnAlbum.setFont(new Font("OCR A Extended", Font.PLAIN, 11));
 		
-		JRadioButton rdbtnMovie_1 = new JRadioButton("Movie");
-		rdbtnMovie_1.setFont(new Font("OCR A Extended", Font.PLAIN, 11));
+		rdbtnMovie = new JRadioButton("Movie");
+		rdbtnMovie.setFont(new Font("OCR A Extended", Font.PLAIN, 11));
+		
+		JCheckBox chckbxShowConsole = new JCheckBox("Show Console");
+		chckbxShowConsole.setSelected(true);
+		chckbxShowConsole.setFont(new Font("OCR A Extended", Font.PLAIN, 11));
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.TRAILING)
@@ -110,59 +113,106 @@ public class MainPanel {
 					.addContainerGap()
 					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_panel.createSequentialGroup()
-							.addComponent(textField, GroupLayout.DEFAULT_SIZE, 181, Short.MAX_VALUE)
+							.addComponent(searchBox, GroupLayout.DEFAULT_SIZE, 223, Short.MAX_VALUE)
 							.addGap(6)
 							.addComponent(chckbxFilterResults)
 							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(btnSearch)
+							.addComponent(searchButton)
 							.addGap(171))
 						.addGroup(gl_panel.createSequentialGroup()
-							.addComponent(rdbtnMovie, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
+							.addComponent(rdbtnSingle, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
 							.addPreferredGap(ComponentPlacement.UNRELATED)
 							.addComponent(rdbtnAlbum, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(rdbtnMovie_1)
-							.addContainerGap(388, Short.MAX_VALUE))))
-				.addGroup(gl_panel.createSequentialGroup()
-					.addComponent(separator, GroupLayout.DEFAULT_SIZE, 582, Short.MAX_VALUE)
-					.addContainerGap())
+							.addComponent(rdbtnMovie)
+							.addContainerGap(396, Short.MAX_VALUE))))
+				.addComponent(separator, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 592, Short.MAX_VALUE)
+				.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(chckbxShowConsole)
+					.addContainerGap(489, Short.MAX_VALUE))
 		);
 		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
+			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_panel.createSequentialGroup()
 							.addGap(30)
 							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(rdbtnMovie, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(rdbtnSingle, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 								.addComponent(rdbtnAlbum)
-								.addComponent(rdbtnMovie_1))
-							.addPreferredGap(ComponentPlacement.RELATED))
+								.addComponent(rdbtnMovie)))
 						.addGroup(gl_panel.createSequentialGroup()
 							.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 								.addComponent(chckbxFilterResults)
-								.addComponent(btnSearch)
-								.addComponent(textField, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE))
+								.addComponent(searchButton)
+								.addComponent(searchBox, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE))
 							.addGap(28)))
-					.addGap(2)
+					.addGap(4)
 					.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(496))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(chckbxShowConsole)
+					.addGap(464))
 		);
 		panel.setLayout(gl_panel);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		frmQuickTorrent.getContentPane().add(scrollPane);
-		
-		JTextPane textPane = new JTextPane();
+		textPane = new JTextPane();
+		//Add actions
+		rdbtnSingle.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				rdbtnAlbum.setSelected(false);
+				rdbtnMovie.setSelected(false);
+				doc = textPane.getStyledDocument();
+				try {
+					if(rdbtnSingle.isSelected())
+						doc.insertString(doc.getLength(),"\nSearch for singles.\n",null );
+				} catch (BadLocationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		rdbtnAlbum.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				rdbtnMovie.setSelected(false);
+				rdbtnSingle.setSelected(false);
+				doc = textPane.getStyledDocument();
+				try {
+					if(rdbtnAlbum.isSelected())
+						doc.insertString(doc.getLength(),"\nSearch for albums.\n",null );
+				} catch (BadLocationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		rdbtnMovie.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				rdbtnAlbum.setSelected(false);
+				rdbtnSingle.setSelected(false);
+				doc = textPane.getStyledDocument();
+				try {
+					if(rdbtnMovie.isSelected())
+						doc.insertString(doc.getLength(),"\nSearch for movies.\n",null );
+				} catch (BadLocationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		chckbxShowConsole.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				consoleOn=!consoleOn;
+				textPane.setVisible(consoleOn);
+				
+			}
+		});
+		//
 		scrollPane.setViewportView(textPane);
-	}
-	private class SwingAction extends AbstractAction {
-		public SwingAction() {
-			putValue(NAME, "SwingAction");
-			putValue(SHORT_DESCRIPTION, "Some short description");
-		}
-		public void actionPerformed(ActionEvent e) {
-		}
+		Actions displayOutput = new Actions();
+		textPane.setText(textPane.getText() + displayText);
 	}
 }
